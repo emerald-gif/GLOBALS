03c3b214c9e3369658790043efbf19261e975310
emerald-gif <ayotomiwaodusanya56@gmail.com>
2025-10-23 06:14:37 +0100

    Update main.js

--- globals 2/public/main.js
+++ globals 2/public/main.js
@@ -6827,7 +6827,7 @@ async function ensureCycleExists(uid) {
       cycleStartDate: today,
       days: Array(7).fill(false),
       status: 'processing',
-      rewardAmount: 300,
+      rewardAmount: 200,
       createdAt: firebase.firestore.FieldValue.serverTimestamp(),
       updatedAt: firebase.firestore.FieldValue.serverTimestamp()
     }, { merge: false });
@@ -6844,7 +6844,7 @@ async function ensureCycleExists(uid) {
       cycleStartDate: today,
       days: Array(7).fill(false),
       status: 'processing',
-      rewardAmount: 300,
+      rewardAmount: 200,
       createdAt: firebase.firestore.FieldValue.serverTimestamp(),
       updatedAt: firebase.firestore.FieldValue.serverTimestamp()
     }, { merge: false });
